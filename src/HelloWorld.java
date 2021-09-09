23d0e63bac3cda2c07ea0499f259f9d70b079b0e
Allan2024 <allanjoseph009@gmail.com>
2021-09-09 15:37:10 +0530

    Add

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+ class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println();
+    }
+
+}
+ //Make project
+ //create class
+ // Make main method
+ // Print something from it
